2e9ed03fc9241b8cb9a18271a717455e1cdf159f
Radeonares32 <bugra.linux.js@gmail.com>
2023-03-07 17:39:33 +0300

    smart contract updated

--- src/program/src/lib.rs
+++ src/program/src/lib.rs
@@ -13,6 +13,29 @@ fn process_instruction(
     accounts:&[AccountInfo],
     instruction_data:&[u8]
 )->ProgramResult {
-    msg!("Hello Solana (From Rust)");
+    let accounts_iter = &mut accounts.iter();
+    let payer = next_account_info(accounts_iter)?;
+    let payee = next_account_info(accounts_iter)?;
+
+    let amount = input
+        .get(..8)
+        .and_then(|slice| slice.try_into().ok())
+        .map(u64::from_le_bytes)
+        .ok_or(ProgramError::InvalidInstructionData)?;
+
+    // let amount = i32::try_from_slice(input);
+
+    msg!("Received request to transfer {:?} lamports from {:?} to {:?}.", 
+        amount, payer.key, payee.key);
+    msg!("  Processing transfer...");
+
+    // Transfer from PAYER to PAYEE a specific amount:
+    invoke(
+        &system_instruction::transfer(payer.key, payee.key, amount),
+        &[payer.clone(), payee.clone()],
+    )?;
+    
+    msg!("Transfer completed successfully.");
     Ok(())
+}
 }
