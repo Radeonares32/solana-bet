faff1e782eb078447706581041123e929611b622
Radeonares32 <bugra.linux.js@gmail.com>
2023-03-07 17:43:39 +0300

    smart contract updated

--- src/program/src/lib.rs
+++ src/program/src/lib.rs
@@ -1,18 +1,29 @@
-use solana_program::{
-    account_info::AccountInfo,
-    entrypoint,
-    entrypoint::ProgramResult,
-    msg,
-    pubkey::Pubkey,
+use {
+    std::convert::TryInto,
+    solana_program::{
+        account_info::{
+            next_account_info, AccountInfo
+        },
+        entrypoint,
+        entrypoint::ProgramResult,
+        msg,
+        program::invoke,
+        program_error::ProgramError,
+        pubkey::Pubkey,
+        system_instruction,
+    },
 };
+
 
 entrypoint!(process_instruction);
 
-fn process_instruction(
-    program_id:&Pubkey,
-    accounts:&[AccountInfo],
-    instruction_data:&[u8]
-)->ProgramResult {
+
+pub fn process_instruction(
+    _program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    input: &[u8],
+) -> ProgramResult {
+    
     let accounts_iter = &mut accounts.iter();
     let payer = next_account_info(accounts_iter)?;
     let payee = next_account_info(accounts_iter)?;
@@ -37,5 +48,4 @@ fn process_instruction(
     
     msg!("Transfer completed successfully.");
     Ok(())
-}
 }
